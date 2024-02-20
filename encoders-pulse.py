e457bc9ef9a3bcfdedee6db1a528dd2abf532a04
James Paskaruk <james@paskaruk.tech>
2024-02-20 17:27:55 +0000

    Crossfade to static works on tuneup only\nProblem solved with python-vlc

---
+++ encoders-pulse.py
@@ -0,0 +1,95 @@
+from threading import Event
+from queue import Queue, LifoQueue
+from gpiozero import RotaryEncoder, Button
+import vlc
+
+
+# pins connected to KY040 rotary encoders
+rotor3 = RotaryEncoder(15,14)
+rotor2 = RotaryEncoder(24,23)
+rotor1 = RotaryEncoder(8,25)
+btn3 = Button(17, pull_up=True)
+btn2 = Button(27, pull_up=True)
+btn1 = Button(7, pull_up=True)
+
+# Event object
+done = Event()
+# Queue (accessible to Event objects) for holding volume during Mute mode
+vol_stash = Queue()
+# Last In, First Out Queue to hold tuning knob events - (direction,current_value) tuples
+tuneup_queue = LifoQueue()
+tunedown_queue = LifoQueue()
+# Static sound player
+static = vlc.MediaPlayer("media/static.wav")
+static.audio_set_volume(0)
+static.play()
+benny = vlc.MediaPlayer("media/benny.mp3")
+benny.audio_set_volume(100)
+benny.play()
+
+def tuningup():
+    if tunedown_queue.empty():
+        tuneup_queue.put(1)
+        curvol = benny.audio_get_volume()
+        newvol = curvol - 10
+        benny.audio_set_volume(newvol)
+        statvol = static.audio_get_volume()
+        newstatvol = statvol + 10
+        static.audio_set_volume(newstatvol)
+    else:
+        tunedown_queue.get()
+    print("Tuneup {} {} Tunedown".format(tuneup_queue.qsize(),tunedown_queue.qsize()))
+
+def tuningdown():
+    if tuneup_queue.empty():
+        tunedown_queue.put(1)
+    else:
+        tuneup_queue.get()
+    print("Tuneup {} {} Tunedown".format(tuneup_queue.qsize(),tunedown_queue.qsize()))
+
+def adjustvolup():
+    current = mixer.getvolume()[0]
+    newvol = current + 1
+    mixer.setvolume(newvol)
+
+def adjustvoldown():
+    current = mixer.getvolume()[0]
+    newvol = current - 1
+    mixer.setvolume(newvol)
+
+def mute():
+    current = mixer.getvolume()[0]
+    vol_stash.put(current)
+    mixer.setvolume(0)
+
+def unmute():
+    stashed_vol = vol_stash.get()
+    mixer.setvolume(stashed_vol)
+
+def togglemute():
+    if vol_stash.empty():
+        mute()
+    else:
+        unmute()
+
+def clockw():
+    print("clockwise!")
+
+def counterw():
+    print("counterclockwise!")
+
+def pus():
+    print("pushed!")
+
+# Knob 1
+rotor1.when_rotated_clockwise = adjustvolup
+rotor1.when_rotated_counter_clockwise = adjustvoldown
+btn1.when_released = togglemute
+
+# Knob 2
+rotor2.when_rotated_clockwise = tuningup
+rotor2.when_rotated_counter_clockwise = tuningdown
+btn2.when_released = pus
+
+
+done.wait()
